0af081e819556660abb48b40b225277d7035431b
kapilsadotra <kapilsadotramato@gmail.com>
2022-11-10 16:31:49 +0530

    Second commit

---
+++ Test.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+                System.out.println("Hello world2");
+
+	}
+
+}
